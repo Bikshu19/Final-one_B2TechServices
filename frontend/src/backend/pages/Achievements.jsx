1a38f769d3ed8909a630a8130bd0c847d7986ba7
Bikshu19 <bikshutulimelli@gmail.com>
2025-10-04 19:58:18 +0530

    Update Achievements.jsx

--- frontend/src/backend/pages/Achievements.jsx
+++ frontend/src/backend/pages/Achievements.jsx
@@ -11,7 +11,7 @@ export default function Achievements({ token }) {
   useEffect(() => {
     const fetchAchievements = async () => {
       try {
-        const res = await fetch("http://localhost:5000/news/achievements", {
+        const res = await fetch("https://final-one-b2techservices.onrender.com/news/achievements", {
           headers,
         });
         if (res.ok) {
