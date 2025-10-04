fc74a10a32dd13798476bed6a51c37209e634478
Bikshu19 <bikshutulimelli@gmail.com>
2025-10-04 20:05:52 +0530

    Update sendotp.jsx

--- frontend/src/backend/pages/sendotp.jsx
+++ frontend/src/backend/pages/sendotp.jsx
@@ -20,7 +20,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://final-one-b2techservices.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
