d7910bf5a84a1dad95a4b6e37a9c3522ebfa3a39
Bikshu19 <bikshutulimelli@gmail.com>
2025-10-04 20:22:55 +0530

    Update Contactform.jsx

--- frontend/src/components/Contactform.jsx
+++ frontend/src/components/Contactform.jsx
@@ -21,7 +21,7 @@ export default function ContactForm() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://final-one-b2techservices.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
